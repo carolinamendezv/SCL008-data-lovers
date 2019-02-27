e5f318b64848928048a83b17b67e937aeb2b361f
FabyTapia <ftapiamorales@gmail.com>
2019-02-26 22:17:13 -0300

    guardando cambios

--- src/main.js
+++ src/main.js
@@ -1,18 +1,30 @@
 /* Manejo del DOM */
 
-const btnWorld = document.getElementById("button-lol-world"); 
-const container=document.getElementById("root");
+
+/*const btnWorld = document.getElementById("button-lol-world")[0]; 
+
+
+let container=document.getElementById("root");
 
 btnWorld.addEventListener('click',() => {
-container.innerHTML=`<h2>LOL world</h2>
-<p>Leage of Legends  es un tipo de juego MOBA
- (MULTIPLAYER ONLINE BATTLE ARENA).
 
+container.innerHTML=`<h2 >LOL world</h2>
+<p class ="subtitle">Leage of Legends  es un tipo de juego MOBA
+ (MULTIPLAYER ONLINE BATTLE ARENA).
 Es un genero de videojuegosque se desarrolla en una arena virtual en conexion con otrs jugadores  
 para cumplir un objetivo claro y especifico, dominar en  batalla al  otro equipo.
 LOL se juega en equipos de a 5 cada jugador elige un personaje. 
 Donde tienen disponible todos los que aparecen en la pagina.
 cada personaje tiene 3 habilidades propias y una 4ta que se conoce como definitiva.
 </p>`
- 
+
 });
+
+const btnCharacters= document.getElementById("button-characters")[1];
+let container2=document.getElementById("root2");
+
+btnCharacters.addEventListener=('click',() =>{
+container2.innerHTML= `<h1>Characters</h1>`
+
+
+});*/
