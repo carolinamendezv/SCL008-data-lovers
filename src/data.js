228fd3c6d1adb140d48423c86ef515d7b3d47321
carolinamendezv <nina.mendezv@gmail.com>
2019-02-27 20:05:30 -0400

    subiendo cambios que git no acepta(prueba)

--- src/data.js
+++ src/data.js
@@ -3,8 +3,11 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const filterData= (data,condition) => {
-  return 'example';
-};
+//const filterData= (data,condition) => {
+  //const filter = data.dataTwo (element =>{
+   // return element.dataTwo ===condition;
+  //})
+  //return 'dataTwo';
+//};
 
-window.example = example;
+//window.filterData= filterData;
