ea7f0d25702260061caac240fbf88a345fcb750a
FabyTapia <ftapiamorales@gmail.com>
2019-03-03 17:47:46 -0300

    modificaciones en los todos los archivos

--- src/data.js
+++ src/data.js
@@ -1,15 +1,1 @@
-/* Manejo de data 
 
- esta es una función de ejemplo
- puedes ver como agregamos la función a nuestro objeto global window*/ 
- 
-const data2 = Object.values(data1);
-
-function filter(data2, condition){
-const dataShow = data2.filter((element)=>{
-    return element[1].tags.includes(condition) === true;
-    
-});
-
-return dataShow;
-}
