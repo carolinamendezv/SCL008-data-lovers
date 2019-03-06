a1a38587f6f711f32f19496dc34d58065547a6c1
carolinamendezv <nina.mendezv@gmail.com>
2019-03-06 15:31:51 -0400

    DATA.SPEC.JS añadimos test de orden pasando ok 4 test

--- test/data.spec.js
+++ test/data.spec.js
@@ -21,3 +21,36 @@ describe('Function filterData', () => {
   });
 
 })
+
+describe('Funcion sortData' , () => {
+const dataSort = [
+ {
+  name : 'Zyra'
+ },
+ {
+  name :'Aatrox'
+ }
+]
+  it('debería retornar en orden alfabético de Z a A el string Zira, luego Aatrox', () => {
+    assert.deepEqual(window.sortData(dataSort,'name','ordenarZa'),
+[
+  {name :'Zyra'},
+  {name : 'Aatrox'}
+])
+
+});
+
+it('deberia retornar en orden alfabético Aatrox y despues Zyra',()=>{
+assert.deepEqual(window.sortData(dataSort,'name','ordenarAz'),
+[
+  {name :'Aatrox'
+},
+{name :'Zyra'
+}
+])
+
+})
+
+});
+
+
